fa9719c3fdea9f11476ad256c9d7bc46004d3fa8
Jamoliddin <sh.jamoliddin2001@gmail.com>
2022-09-20 21:59:48 +0500

    testing someone function

--- app/src/main/java/tj/jamoliddin/biometricauthentication/domain/biometric/CryptographyManager.kt
+++ app/src/main/java/tj/jamoliddin/biometricauthentication/domain/biometric/CryptographyManager.kt
@@ -145,5 +145,5 @@ data class CiphertextWrapper(val ciphertext: ByteArray, val initializationVector
 
 fun main(){
     val a = "something"
-    print(a.substringBefore("t"))
+    print(a.substringBefore("h"))
 }
